1a6e43cc4e9d79b232a85c432e189ca4dda58720
Wilianto Indrawan <wilianto.i@go-jek.com>
2018-02-25 22:27:15 +0700

    update user db migration

--- migration/1519461009_create_users_table.up.sql
+++ migration/1519461009_create_users_table.up.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `users` (
-  `id` integer PRIMARY KEY,
-  `username` varchar(32),
-  `password` varchar(32),
-  `name` varchar(100),
-  CONSTRAINT `i_unique_username` UNIQUE(`username`)
+CREATE TABLE users (
+  id serial,
+  username varchar(32),
+  password varchar(32),
+  name varchar(100),
+  CONSTRAINT i_unique_username UNIQUE(username)
 )
