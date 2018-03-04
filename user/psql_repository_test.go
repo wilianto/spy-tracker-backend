cc99023a4a4373ce58c228bcc595f4227ba17a95
Wilianto Indrawan <indrawan.wilianto@gmail.com>
2018-03-04 14:24:48 +0700

    add user psql repository negative test

--- user/psql_repository_test.go
+++ user/psql_repository_test.go
@@ -1,6 +1,7 @@
 package user_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,7 +9,7 @@ import (
 	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
-func TestStore(t *testing.T) {
+func TestStore_Success(t *testing.T) {
 	usr := &user.User{
 		Username: "wilianto",
 		Password: "hash_password",
@@ -31,4 +32,28 @@ func TestStore(t *testing.T) {
 	lastID, err := repo.Store(usr)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(5), lastID)
+}
+
+func TestStore_Error(t *testing.T) {
+	usr := &user.User{
+		Username: "wilianto",
+		Password: "hash_password",
+		Name:     "Wilianto Indrawan",
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Error when creating sql mock: %s", err.Error())
+	}
+	defer db.Close()
+
+	query := "INSERT INTO users \\(username, password, name\\) VALUES \\(\\$1, \\$2, \\$3\\) RETURNING id"
+	mock.ExpectQuery(query).
+		WithArgs(usr.Username, usr.Password, usr.Name).
+		WillReturnError(errors.New("Insert error"))
+
+	repo := user.NewPsqlRepository(db)
+	lastID, err := repo.Store(usr)
+	assert.Equal(t, "Insert error", err.Error())
+	assert.Equal(t, int64(0), lastID)
 }
